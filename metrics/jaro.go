3a623f29c1ae5d1fb9d160bb5ac8516bbf038089
Georgii Nevzorov <nevzorovgosha@mail.ru>
2023-04-21 10:24:50 +0300

    Fix module name

--- metrics/jaro.go
+++ metrics/jaro.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/adrg/strutil/internal/mathutil"
+	"github.com/snapsuzun/strutil/internal/mathutil"
 )
 
 // Jaro represents the Jaro metric for measuring the similarity
