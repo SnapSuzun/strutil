35ab170de07d1434cbb3ea7d64e6b85145e87564
Georgii Nevzorov <nevzorovgosha@mail.ru>
2023-04-21 14:03:35 +0300

    Fix module name

--- metrics/levenshtein.go
+++ metrics/levenshtein.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/adrg/strutil/internal/mathutil"
+	"github.com/snapsuzun/strutil/internal/mathutil"
 )
 
 // Levenshtein represents the Levenshtein metric for measuring the similarity
